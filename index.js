4486bd073a9320c5aac0e0d0bac0dcbdea0a301e
ChaseHaluptzok <c.haluptzok7@gmail.com>
2020-04-01 12:15:39 -0700

    fix for loop to walk length of array

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 exports.PolyverseSort = function(toSort){
     var ret = [];
-    for(let i = 0; i<toSort; i++){
+    for(let i = 0; i<toSort.length; i++){
         ret.push(toSort[i].toLowerCase());
     }
     ret.sort();
-    console.log(ret);
     return ret;
 }
